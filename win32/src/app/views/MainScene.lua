91b8638c44e917e87444af44889ee392b3669f10
yangquanyu <yangquanyu@yy.com>
2019-07-19 14:53:06 +0800

    整理图片加载方式.

--- win32/src/app/views/MainScene.lua
+++ win32/src/app/views/MainScene.lua
@@ -2,7 +2,7 @@
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 local function createNormalBtn(node,imageName1,imageName2,imageName3, x, callback)
-	local btn = ccui.Button:create(imageName1,imageName2,imageName3)
+	local btn = ccui.Button:create(imageName1,imageName2,imageName3,ccui.TextureResType.plistType)
 	btn:addClickEventListener(callback)
 	btn:setScale(1.5)
 	btn:move(x)
@@ -18,15 +18,14 @@ end
 	
 function MainScene:onCreate()
     -- 开始背景图.
-    display.newSprite("startBK.jpg")
-        :move(display.center)
-        :addTo(self)
+    cc.SpriteFrameCache:getInstance():addSpriteFrames("common.plist")
+    self.spriteBK = cc.Sprite:createWithSpriteFrameName("startBK.jpg")
+    self.spriteBK:move(display.center)
+    self.spriteBK:addTo(self)
 
 	-- 开始游戏按钮.
     createNormalBtn(self,"startBtn.png","startBtn_selected.png","startBtn_disable.png", display.center,
 		handler(self, self.onStartClicked))
-
-    cc.SpriteFrameCache:getInstance():addSpriteFrames("common.plist")
 end
 
 return MainScene
